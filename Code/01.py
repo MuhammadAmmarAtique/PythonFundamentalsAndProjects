4001ad6f1f8b2364029a08144985f5f60dcb7c28
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-03-12 00:50:13 +0500

    Started learning basics

--- Code/01.py
+++ Code/01.py
@@ -1,1 +1,12 @@
-# 1)Variables
+# 1)Variables + Datataypes
+FirstName = "Tony"
+LastName = "Stark"
+age = 21
+isGenius = True
+
+# print(FirstName + ""  + LastName)  #string concatenation
+
+# 2)Taking input from user
+User = input("Enter your name :) ")
+UserSuperHeroName = input("Who is your favourite super hero? ")
+print("Okay " + User + " So you SuperHero is " + UserSuperHeroName + " Thats Great!")
