7d9dfd567b6c43e16c0b5e048188f01f966ae10b
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-03-12 17:49:09 +0500

    learned some basics concepts

--- Code/01.py
+++ Code/01.py
@@ -1,12 +1,51 @@
-# 1)Variables + Datataypes
-FirstName = "Tony"
-LastName = "Stark"
-age = 21
-isGenius = True
+# 1) Variables + Datataypes:-
+
+# FirstName = "Tony"
+# LastName = "Stark"
+# age = 21
+# isGenius = True
 
 # print(FirstName + ""  + LastName)  #string concatenation
 
-# 2)Taking input from user
-User = input("Enter your name :) ")
-UserSuperHeroName = input("Who is your favourite super hero? ")
-print("Okay " + User + " So you SuperHero is " + UserSuperHeroName + " Thats Great!")
+# 2)Taking input from user:-
+
+# User = input("Enter your name :) ")
+# UserSuperHeroName = input("Who is your favourite super hero? ")
+# print("Okay " + User + " So you SuperHero is " + UserSuperHeroName + " Thats Great!")
+
+# 3) Type conversions:-
+
+# e.g 1
+# num1 = input("Enter first number: ")
+# num2 = input("Enter second number: ")
+# sum = int(num1) + int(num2)
+
+# print ("The sum of two numbers is " + str(sum))
+
+# e.g 2
+# RafehExactAge = input("Enter's exact Rafeh age: ")
+# AmmarExactAge = input("Enter's exact Ammar age: ")
+
+# difference = float(RafehExactAge) - float(AmmarExactAge)
+# print(difference)
+
+# 4) String Methods:-
+
+# name = "Muhammad Ammar Atique"
+# print (name.upper())
+# print (name.lower())
+# print (name.find("A"))
+# print (name.replace("Ammar", "Rafeh"))
+# print (name) #original string is unchanged
+
+# 5) "in" Keywords:-
+
+# name = "Muhammad Ammar Atique"
+# print("m" in name)
+# print("s" in name)
+# print("Ammar" in name)
+# print("Rafeh" in name)
+
+
+
+
