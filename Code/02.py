917f1ffc5fbefc4884afa4966087af641f3b4163
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-03-12 21:34:19 +0500

    Learned more basics about python language

---
+++ Code/02.py
@@ -0,0 +1,20 @@
+# 1) Arithematic Operators ( +, -, *, /, % ,**)
+
+# print(5 + 2)
+# print(5 - 2)
+# print(5 * 2)
+# print(5 / 2)
+# print(5 % 2)
+# print(5 ** 2)
+
+# 2)Shortcuts
+
+# a)
+# i = 5
+# i = i + 10 
+# print(i)
+
+# #  b)
+# i = 5
+# i += 10
+# print(i)
