a2d0a60d56ab6288d34b2a2a992ab9b341b6cac5
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-03-15 23:36:21 +0500

    learned about break and continue statement

---
+++ Code/06.py
@@ -0,0 +1,18 @@
+# 1) Break and Continue Statements
+
+students = ["ammar","rafeh","moiz","ali","umer"]
+
+# a) break statement
+
+# for i in students:
+#     if i == "moiz":
+#      break
+#     print(i)
+
+# a) continue statement
+    
+for i in students:
+    if i == "moiz":
+     continue
+    print(i)
+    
