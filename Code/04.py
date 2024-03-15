f73f834f3af754d3dd5997e4a76df96769221ee5
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-03-15 18:12:37 +0500

    Revised revious topics and learned loops

---
+++ Code/04.py
@@ -0,0 +1,26 @@
+# 1) Range
+# num = range(5)
+# print(num)
+
+# 2)While loop
+
+# a)
+
+# i=1
+# while i <= 5:         #This line is strating and ending condition for a loop
+#    print(i * "*")
+#    i= i+1
+
+# b)
+
+# j=5
+# while j >= 0:
+#    print(j * "*")
+#    j= j-1   
+
+# 3)For loop
+
+myrange = range(5)
+
+for number in myrange:
+   print(number)      
