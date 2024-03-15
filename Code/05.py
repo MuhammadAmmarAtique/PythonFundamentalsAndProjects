5964adb24a8af688b5aac63976dad72e5985a780
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-03-15 22:59:28 +0500

    learned about lists

---
+++ Code/05.py
@@ -0,0 +1,18 @@
+# 1) Lists in python
+marks = [11,22,33,45,65,76]
+# print(marks)
+# print(marks[2])
+# print(marks[-6])  #negative indexing
+# print(marks[1:5]) #getting a range of data from list using indexing (5th index will not be included)
+
+# 2) Accessing Lists elements using for loop
+# for elem in marks:
+#     print(elem)
+
+# 3) Accessing Lists elements using while loop
+i=0
+
+while i <  len(marks):
+    print(marks[i])
+    i = i+1
+   
