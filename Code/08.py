93712d0b51259c8258cf636ea91d7faf5c64ee92
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-03-17 17:21:57 +0500

    Learned aboout Functions in Python and Completed Course

---
+++ Code/08.py
@@ -0,0 +1,17 @@
+# 1)Functions
+# a)Module Functions
+
+# import math
+# print(dir(math))
+
+# from math import *
+# print(pi)
+# print(float(123.343))
+
+# b)User defined Functions
+
+def printSum (num1,num2):
+    result = num1 + num2
+    print(result)
+
+printSum(23,34)    
