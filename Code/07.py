85b58d2d1f0d7c12b800f6e2ae1b67ffe83f4a0b
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-03-17 16:55:59 +0500

    Learned about Tupples, Sets and Dictionary.

---
+++ Code/07.py
@@ -0,0 +1,26 @@
+# 1) Tupples
+
+# marks  = (100,23,45,76,76)
+# # marks[2] = 21                #tupples are immutable
+# print(marks)
+# print(marks.count(76))
+# print(marks.index(23))
+
+# 2) Sets
+# marks  = {100,23,76, 45,45}
+# print(marks)
+
+# for elements in marks:
+#     print(elements)
+
+# 3)Dictionary
+# marks = {
+#    "bio":78,
+#    "chemistry" :99,
+#    "physics" : 29,
+# }
+
+# print(marks["bio"])
+# print(marks)
+# marks["physics"] = 89
+# print(marks)
